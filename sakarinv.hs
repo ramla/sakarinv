e8b171031f86fba0b1fa01f7d9dc110f5b8fc064
ramla <grammler@gmail.com>
2023-03-17 15:24:58 +0200

    current progress. oopsie, forgot to add the art files in earlier commits

--- sakarinv.hs
+++ sakarinv.hs
@@ -13,10 +13,7 @@ import System.Random
 -- h,j,k,l,enter
 --
 -- todo: 
--- silmät ainaki lopuks
--- silmien liike? 
 -- pään pudistus? 
--- sakari joka kuvaan?
 -- artin luku erikseen alukss etteio liikaa io
 -- artin vakiointi? offset-luvut .arttiin?
 
@@ -26,76 +23,94 @@ main =
         hSetBuffering stdin NoBuffering
         hideCursor
         setTitle "Sakarin villapaitapelin kosto"
-        intro
-        loop
+        sakari <- readFile "sakari.art"
+        villapaita <- readFile "villapai.art"
+        intro sakari
+        loop sakari villapaita
             where
-            loop = do
-                    sx <- randomRIO (0,69) :: IO Int
+            loop sakari villapaita = do
+                    -- set sakari position
+                    sx <- randomRIO (10,69) :: IO Int
                     sy <- randomRIO (-13,0) :: IO Int
-                --    let sx = (69)
-                --        sy = (-13)
+                    --let sx = (69) -- debug sakari position
+                    --    sy = (-13)
                     c <- getChar            
                     clearFromCursorToLineBeginning
                     cursorBackward 1
                     when (c /= 'q') $ do
                     case (toLower c) of
-                        'y' -> trackShit 0 0 sx sy
+                        'y' -> trackShit 0 0 sx sy sakari villapaita
                         'n' -> havisitPelin
-                        _   -> do putStrLn "Ei noin"
-      
-trackShit x y sx sy =
+                        _   -> do eiNoin
+
+trackShit x y sx sy sakari villapaita =
     do  
         clearScreen
-        putStrLn (show x ++ " " ++ show y ++ " " ++ show sx ++ " " ++ show sy) 
-        putView x y sx sy
-        if (x == sx && y == sy) then hihihi
+      --  putStrLn (show x ++ " " ++ show y ++ " " ++ show sx ++ " " ++ show sy) -- debug movement/position
+        putView x y sx sy sakari villapaita
+        if (x == sx && y == sy) then hiHiHi
         else
             do
                 c <- getChar
                 case (toLower c) of
-                    'h' -> trackShit (x-1) y sx sy 
-                    'j' -> trackShit x (y-1) sx sy 
-                    'k' -> trackShit x (y+1) sx sy 
-                    'l' -> trackShit (x+1) y sx sy 
+                    'h' -> trackShit (x-1) y sx sy sakari villapaita 
+                    'j' -> trackShit x (y-1) sx sy sakari villapaita
+                    'k' -> trackShit x (y+1) sx sy sakari villapaita
+                    'l' -> trackShit (x+1) y sx sy sakari villapaita 
                     'q' -> return ()
-                    _   -> havisitPelin
-intro = 
-    do  putStrLn "Sakarin villapaitapelin kosto"
+                    _   -> eiNoin
+intro sakari =
+    do  clearScreen
+        putStrLn "Sakarin villapaitapelin kosto"
         putStrLn ""
+        putStrLn ""
+        putStrLn sakari
         putStrLn "Pue sakarille villapaita y/n"
-havisitPelin = 
-    do  putStrLn "Hävisit pelin"
         putStrLn ""
-hihihi =
-    do  putStrLn "Hihihi, kutittaa!"
+        putStrLn ""
+
+havisitPelin = 
+    do  lose <- readFile "lose.art"
+        clearScreen
+        putStrLn lose
+        putStrLn "Hävisit pelin"
+        putStrLn ""
+        putStrLn ""
+
+hiHiHi =
+    do  win <- readFile "win.art"
+        clearScreen
+        putStrLn win
+        putStrLn "\"Hihihi, kutittaa!\""
         putStrLn ""
         putStrLn "Voitit pelin"
+        putStrLn ""
+        putStrLn ""
 
-putView :: Int -> Int -> Int -> Int -> IO ()
-putView x y sx sy = 
-        do contents <- readFile "test"
-           villapaita <- readFile "test2"
+eiNoin =
+    do  putStrLn "Ei noin!"
+        putStrLn ""
+        putStrLn ""
+        putStrLn ""
+
+putView :: Int -> Int -> Int -> Int -> String -> String -> IO ()
+putView x y sx sy sakari villapaita =
            let positionedContents = 
-                merge (lines positionedVillapaita) (lines (addRows (sy) (addColumns sx contents)))
+                merge (lines positionedVillapaita) (lines (addRows (sy) (addColumns sx sakari)))
                 where positionedVillapaita = addRows y (addColumns x villapaita)
                 in putStrLn (unlines positionedContents)
-           return ()
 
--- canvas min sakarin sij+8,sakarin sij+4 (80x24 sakarimaxsij. 72,-20, paitsiettä
--- sakarin ivllapaita vie vielä +3rivii ja on 11 levee (+3) eli 69,-17 (-debug 1, -input 1) -> 69,15
-
+--merging arts one row at a time
 merge [] [] = []
 merge [] bot = bot 
 merge top [] = top 
 merge (toprow:top) (botrow:bot) = mergeRow toprow botrow : merge top bot 
 
+--merging rows by comparing chars
 mergeRow [] [] = []
 mergeRow [] botrow = botrow
 mergeRow toprow [] = toprow
 mergeRow (t:toprow) (b:botrow) = if t > b then t:mergeRow toprow botrow else b:mergeRow toprow botrow
-
-emptyLengthAtRowBeginning row = length (dropWhile (\x -> not $ elem x " ") . takeWhile (\x -> elem x " ") $ row)
-emptyLengthAtRowEnd row = emptyLengthAtRowBeginning (reverse row)
 
 addColumns 0 contents = contents
 addColumns n contents = addColumns (n-1) (unlines (map (\x -> " " ++ x) (lines contents)))
