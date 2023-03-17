9579a529f6de87a266c5b2695ff7c802dd4ac0df
ramla <grammler@gmail.com>
2023-03-17 15:18:56 +0200

    backup7: woolly jumper is moving

--- sakarinv.hs
+++ sakarinv.hs
@@ -4,6 +4,8 @@ import System.IO
 import System.Console.ANSI --clearScreen
 import Control.Monad -- when
 import Data.Char --toLower
+import Control.Exception --catch
+import System.Random
 
 -- sakarin villapaitapeli cli 2023 remix
 --
@@ -26,27 +28,30 @@ main =
         loop
             where
             loop = do
+                    sx <- randomRIO (0,10) :: IO Int
+                    sy <- randomRIO (0,10) :: IO Int
                     c <- getChar
                     when (c /= 'q') $ do
                     case (toLower c) of
-                        'y' -> trackShit 0 0
+                        'y' -> trackShit 0 0 sx sy
                         'n' -> havisitPelin
                         _   -> putStrLn "Ei noin"
       
-trackShit x y =
+trackShit x y sx sy =
     do  
         clearScreen
-        putStrLn ("-- trackShit"++(show x)++(show y))
-        if (x == 5) then hihihi
+        putStrLn (show x ++ " " ++ show y ++ " " ++ show sx ++ " " ++ show sy)
+        putView x y sx sy
+        if (x == sx && y == sy) then hihihi
         else
             do
                -- hSetBuffering stdin NoBuffering
                 c <- getChar
                 case (toLower c) of
-                    'h' -> trackShit (x-1) y
-                    'j' -> trackShit x (y-1)
-                    'k' -> trackShit x (y+1)
-                    'l' -> trackShit (x+1) y
+                    'h' -> trackShit (x-1) y sx sy 
+                    'j' -> trackShit x (y-1) sx sy 
+                    'k' -> trackShit x (y+1) sx sy 
+                    'l' -> trackShit (x+1) y sx sy 
                     'q' -> return ()
                     _   -> havisitPelin
 intro = 
@@ -60,6 +65,48 @@ hihihi =
     do  putStrLn "Hihihi, kutittaa!"
         putStrLn ""
         putStrLn "Voitit pelin"
+
+putView :: Int -> Int -> Int -> Int -> IO ()
+putView x y sx sy = 
+        do contents <- readFile "test"
+           villapaita <- readFile "test2"
+           let positionedContents = 
+                merge (lines positionedVillapaita) (lines (addRows (y) (addColumns x contents)))
+                where positionedVillapaita = addRows sy (addColumns sx villapaita)
+                in putStrLn (unlines positionedContents)
+           return ()
+
+merge [] [] = []
+merge [] bot = bot 
+merge top [] = top 
+merge (toprow:top) (botrow:bot) = mergeRow toprow botrow : merge top bot 
+
+mergeRow [] [] = []
+mergeRow [] botrow = botrow
+mergeRow toprow [] = toprow
+mergeRow (t:toprow) (b:botrow) = if t > b then t:mergeRow toprow botrow else b:mergeRow toprow botrow
+
+emptyLengthAtRowBeginning row = length (dropWhile (\x -> not $ elem x " ") . takeWhile (\x -> elem x " ") $ row)
+emptyLengthAtRowEnd row = emptyLengthAtRowBeginning (reverse row)
+
+addColumns 0 contents = contents
+addColumns n contents = addColumns (n-1) (unlines (map (\x -> " " ++ x) (lines contents)))
+
+addRows n contents = case n of _
+                                | n < 0 -> addRowsBefore (-n) contents
+                                | n > 0 -> addRowsAfter n contents
+                                | n == 0 -> contents
+
+addRowsBefore 0 contents = contents
+addRowsBefore n contents = addRowsBefore (n-1) (unlines ("" : lines contents))
+
+addRowsAfter 0 contents = contents
+addRowsAfter n contents = addRowsAfter (n-1) (unlines (reverse ("" : reverse (lines contents))))
+
+handler :: IOError -> IO ()
+handler e = putStrLn "File errorr"
+
+
 
 -- readPlayer :: IO ()
 -- readPlayer = do
